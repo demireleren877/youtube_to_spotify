75d0cb636e20e9ee8fb05be39ba458f8a4c827ad
EREN <EREN@DESKTOP-DRT5BB2>
2023-12-03 22:25:07 +0300

    updates

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
-from os import error
+import os
+import spotipy
+import uuid
+from flask_session import Session
 from flask import Flask,render_template,request,redirect,session,url_for
 import controller
-app = Flask(__name__)
-app.secret_key = 'sASDafsdfj'
-app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
 
 @app.route('/')
-def login():
-    sp_oauth = controller.create_spotify_oauth()
-    auth_url = sp_oauth.get_authorize_url()
-    print(auth_url)
-    return redirect(auth_url)
+def index():
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
 
-@app.route('/redirect')
-def redirectPage():
-    sp_oauth = controller.create_spotify_oauth()
-    session.clear()
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    session['token_info'] = token_info
-    return redirect(url_for('home', _external=True))
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private user-library-read playlist-modify-public',
+            client_id="12fe8879174845bf94e9de513fe889c7",
+            client_secret="15ffde4f252e482488d723204e91a21c",
+            redirect_uri='http://127.0.0.1:5000/',
+            cache_path=session_cache_path(),show_dialog=True
+    )
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.get_cached_token():
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 4. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return redirect("/home")
+
 
 @app.route('/home',methods=['GET','POST'])
 def home():
--- controller.py
+++ controller.py
@@ -1,63 +1,42 @@
 import requests
 import json
-from pytube import YouTube, Playlist
+from pytube import Playlist
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from flask import Flask, url_for, session, request, redirect
+from flask import url_for, session,redirect
 from urllib.parse import urlencode
-from spotipy.oauth2 import SpotifyOAuth
-import time
-import socket
+import spotipy
 
-# hostname = socket.gethostname()
-# ipaddress = socket.gethostbyname(hostname)
-# path= str(ipaddress)+".txt"
+caches_folder = './.spotify_caches/'
+def session_cache_path():
+    return caches_folder + session.get('uuid')
 pl_link_is_valid=False
-def get_token():
-    token_valid = False
-    token_info = session.get("token_info", {})
 
-    # Checking if the session already has a token stored
-    if not (session.get('token_info', False)):
-        token_valid = False
-        return token_info, token_valid
-
-    # Checking if token has expired
-    now = int(time.time())
-    is_token_expired = session.get('token_info').get('expires_at') - now < 60
-
-    # Refreshing token if it has expired
-    if (is_token_expired):
-        sp_oauth = create_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
-
-    token_valid = True
-    return token_info, token_valid
-
-
-def create_spotify_oauth():
-    return SpotifyOAuth(
+def authObject():
+    return spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private user-library-read playlist-modify-public',
             client_id="12fe8879174845bf94e9de513fe889c7",
             client_secret="15ffde4f252e482488d723204e91a21c",
-            redirect_uri=url_for('redirectPage', _external=True),
-            scope="user-library-read playlist-modify-public")
+            redirect_uri='http://127.0.0.1:5000/',
+            cache_path=session_cache_path(),show_dialog=True
+    )
 
 #Spotify create playlis
 def createPlaylist(playlist_name):
-    session['token_info'], authorized = get_token()
-    session.modified = True
-    if not authorized:
+    auth_manager = authObject()
+    if not auth_manager.get_cached_token():
         return redirect('/')
-    header= {"Authorization": f"Bearer {session.get('token_info').get('access_token')}", "Content-Type": "application/json"}
+    header= {"Authorization": f"Bearer {auth_manager.get_cached_token()['access_token']}", "Content-Type": "application/json"}
     data={"name": playlist_name, "description": "This is a new playlist"}
-    url="https://api.spotify.com/v1/users/8hon3mc37max0pvvkn7kyjx1o/playlists"
+    url="https://api.spotify.com/v1/users/{}/playlists".format(getCurrentUser()) #8hon3mc37max0pvvkn7kyjx1o
     r=requests.post(url, headers=header, data=json.dumps(data))
-    print(r.json())
     if "id" in r.json().keys():
         return r.json()["id"]
 
 #Spotify search request
 def searchSong(song_name):
-    header={"Authorization":f"Bearer {session.get('token_info').get('access_token')}"}
+    auth_manager = authObject()
+    if not auth_manager.get_cached_token():
+        return redirect('/')
+    header={"Authorization":f"Bearer {auth_manager.get_cached_token()['access_token']}"}
     endpoint = "https://api.spotify.com/v1/search"
     data=urlencode({"q":song_name, "type":"track"})
     lookup_url=f"{endpoint}?{data}"
@@ -65,35 +44,50 @@ def searchSong(song_name):
     if "tracks" in r.json().keys():
         return r.json()["tracks"]["items"][0]["uri"]
 
+def getCurrentUser():
+    auth_manager = authObject()
+    if not auth_manager.get_cached_token():
+        return redirect('/')
+    header={"Authorization":f"Bearer {auth_manager.get_cached_token()['access_token']}"}
+    endpoint = "https://api.spotify.com/v1/me"
+    r = requests.get(endpoint, headers=header)
+    return r.json()["id"]
 
 
 #YT Get song names
 def getSpotifyUris(playlist_link):
+    playlist = Playlist(playlist_link)
     if (playlist_link.__contains__("https://www.youtube.com/playlist?list=")):
         video_links = Playlist(playlist_link).video_urls
         if len(video_links) > 0:            
             global pl_link_is_valid
-            pl_link_is_valid=True            
-            def get_video_title(link):
-                title = YouTube(link).metadata  
-                if title.metadata.__len__() != 0:
-                    return title                
+            pl_link_is_valid=True      
+            names = []      
+            def get_video_title():
+                for video in playlist.videos:
+                    name = video.title
+                    names.append(name)
+                return names
+                               
             processes = []  
             uris=[]
             with ThreadPoolExecutor(max_workers=10) as executor:
-                for url in video_links:
-                    processes.append(executor.submit(get_video_title, url))
-            for task in as_completed(processes):
-                if task.result() is not None:
-                    uris.append(searchSong(task.result()[0]["Song"]))       
+                processes.append(executor.submit(get_video_title))
+            for tasks in as_completed(processes):
+                if tasks.result() is not None:
+                    for task in tasks.result():
+                        uris.append(searchSong(task))
             return uris
 
 def addSongsToPlaylist(playlist_id,playlist_link):
-    header={"Authorization":f"Bearer {session.get('token_info').get('access_token')}"}
+    auth_manager = authObject()
+    if not auth_manager.get_cached_token():
+        return redirect('/')
+    header={"Authorization":f"Bearer {auth_manager.get_cached_token()['access_token']}"}
     endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     data={"uris": getSpotifyUris(playlist_link)}
     r = requests.post(endpoint, headers=header, data=json.dumps(data))
-    return r;
+    return r
 
 def main(playlist_link,playlist_name):  
     getSpotifyUris(playlist_link)
