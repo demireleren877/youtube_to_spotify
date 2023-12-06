508df967fef7ac17d791001aabc139b47dded9d7
Eren Demirel <l5819033@std.yildiz.edu.tr>
2023-12-06 23:44:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def home():
         return render_template("index.html",errorTitle="")
  
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='8000',debug=True)
+    app.run(host='0.0.0.0',port=8000,debug=True)
